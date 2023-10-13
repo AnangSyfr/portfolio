373f1c92eb804c6396dbb7d1019c24b2ba242b22
Anang Syaifur <anangsyfr123@gmail.com>
2023-10-13 22:28:30 +0700

    feat: add more portfolios

--- src/app/(default_site)/contact/page.tsx
+++ src/app/(default_site)/contact/page.tsx
@@ -1,13 +1,31 @@
+import Link from "next/link";
 import React from "react";
+import { GitHub, Facebook, Instagram, Linkedin } from "react-feather";
 
 const Contact = () => {
   return (
     <div className="w-full">
       <ul>
-        <li>Github</li>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>LinkedIn</li>
+        <li className="mb-3">
+          <Link href={""} className="inline-block w-auto">
+            <GitHub className="inline" /> Github
+          </Link>
+        </li>
+        <li className="mb-3">
+          <Link href={""} className="inline-block w-auto">
+            <Facebook className="inline" /> Facebook
+          </Link>
+        </li>
+        <li className="mb-3">
+          <Link href={""} className="inline-block w-auto">
+            <Instagram className="inline" /> Instagram
+          </Link>
+        </li>
+        <li className="mb-3">
+          <Link href={""} className="inline-block w-auto">
+            <Linkedin className="inline" /> Linkedin
+          </Link>
+        </li>
       </ul>
     </div>
   );
--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -4,24 +4,12 @@ export const portfolios = [
     data: [
       {
         id: 1,
-        name: "Mbak Kasir",
-        description: "Sistem Point Of Sale untuk restoran",
-        images: ["/portfolio1.png"],
+        name: "Kegiatan Harian",
+        description: "lorem ipsum dolor sit amet",
+        images: ["/akh-1.png", "/akh-2.png", "/akh-3.png"],
       },
       {
         id: 2,
-        name: "Panorama Bimbel",
-        description: "Sistem Manajemen untuk lembaga bimbel",
-        images: ["/portfolio2.png"],
-      },
-      {
-        id: 3,
-        name: "Sniper",
-        description: "Sistem Notifikasi perkara untuk pengadilan agama",
-        images: ["/portfolio3.png"],
-      },
-      {
-        id: 4,
         name: "Mauju Time Tracker",
         description: "lorem ipsum dolor sit amet",
         images: [
@@ -30,6 +18,12 @@ export const portfolios = [
           "/mauju-timetracker-3.png",
           "/mauju-timetracker-4.png",
         ],
+      },
+      {
+        id: 3,
+        name: "JAS Pati",
+        description: "lorem ipsum dolor sit amet",
+        images: ["/jas-1.png", "/jas-2.png", "/jas-3.png", "/jas-4.png"],
       },
     ],
   },
@@ -45,6 +39,51 @@ export const portfolios = [
           "/mauju-invoice-2.png",
           "/mauju-invoice-3.png",
           "/mauju-invoice-4.png",
+        ],
+      },
+      {
+        id: 6,
+        name: "Panorama Bimbel",
+        description: "Sistem Manajemen untuk lembaga bimbel",
+        images: [
+          "/bimbel-1.png",
+          "/bimbel-2.png",
+          "/bimbel-3.png",
+          "/bimbel-4.png",
+        ],
+      },
+      {
+        id: 7,
+        name: "Mbak Kasir",
+        description: "Sistem Point Of Sale untuk restoran",
+        images: ["/portfolio1.png"],
+      },
+      {
+        id: 8,
+        name: "Sniper",
+        description: "Sistem Notifikasi perkara untuk pengadilan agama",
+        images: ["/portfolio3.png"],
+      },
+      {
+        id: 9,
+        name: "Paniai Mandiri",
+        description: "lorem ipsum dolor sit amet",
+        images: [
+          "/paniai-1.png",
+          "/paniai-2.png",
+          "/paniai-3.png",
+          "/paniai-4.png",
+        ],
+      },
+      {
+        id: 10,
+        name: "Tree Crowdfunding",
+        description: "lorem ipsum dolor sit amet",
+        images: [
+          "/crowd-1.png",
+          "/crowd-2.png",
+          "/crowd-3.png",
+          "/crowd-4.png",
         ],
       },
     ],
